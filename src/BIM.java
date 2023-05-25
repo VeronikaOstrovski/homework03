409f6efb97d2952d343286c93bb3af7de1feaf7f
veronikaostrovski <veronika.turkey@gmail.com>
2023-05-26 01:17:19 +0300

    task 2 done

---
+++ src/BIM.java
@@ -0,0 +1,14 @@
+// task 2
+public class BIM {
+
+    public static void main(String[] args) {
+
+        double WEIGHT = 52.0;
+        double HEIGHT = 1.73;
+        double BMI = WEIGHT / (HEIGHT * HEIGHT);
+
+        System.out.println(BMI = WEIGHT / (HEIGHT * HEIGHT));
+
+    }
+}
+
