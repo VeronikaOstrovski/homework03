2a806fe0b5af562a48f6748c0c7a4b6077f382b2
veronikaostrovski <veronika.turkey@gmail.com>
2023-05-26 01:18:19 +0300

    task 4 done

---
+++ src/Overflow.java
@@ -0,0 +1,12 @@
+// task 4
+public class Overflow {
+    public static void main(String[] args) {
+
+        int a = 2147483647;
+        int b = 1;
+        int c = a + b;
+
+        System.out.println(c = a + b);
+
+    }
+}
