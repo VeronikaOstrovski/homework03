ed2a4e3ff0828cf63e5da7db3c72a48b7165d702
veronikaostrovski <veronika.turkey@gmail.com>
2023-05-26 01:39:42 +0300

    task 6 done

---
+++ src/Age.java
@@ -0,0 +1,16 @@
+// task 6
+public class Age {
+
+    public static void main(String[] args) {
+
+        int AGE1 = 25;
+        int AGE2 = 18;
+        boolean notEqual1 = AGE2 > AGE1;
+        boolean notEqual2 = AGE2 == AGE1;
+
+        System.out.println(notEqual1 = AGE2 > AGE1);
+        System.out.println(notEqual2 = AGE2 == AGE1);
+
+    }
+}
+
