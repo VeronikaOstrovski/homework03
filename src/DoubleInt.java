ae8815ba4f2b34b27c8731f508d86f2d848ab9a3
veronikaostrovski <veronika.turkey@gmail.com>
2023-05-26 01:45:40 +0300

    task 5 done

---
+++ src/DoubleInt.java
@@ -0,0 +1,14 @@
+// task 5
+public class DoubleInt {
+
+    public static void main(String[] args) {
+
+        double a = 3/5;
+        int b = (int) a;
+
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+}
+
