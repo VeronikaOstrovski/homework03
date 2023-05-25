0fed4fbb3fbcce2817c72e9d53c5a52b99417ee0
veronikaostrovski <veronika.turkey@gmail.com>
2023-05-26 01:18:02 +0300

    task 3 done

---
+++ src/Temperature.java
@@ -0,0 +1,14 @@
+// task 3
+public class Temperature {
+
+    public static void main(String[] args) {
+
+        double degreesCelsius = 27.0;
+        double degreesFahrenheit = 90.0;
+        double tempDegreesFahrenheit = (9/5) * degreesCelsius + 32.0;
+
+        System.out.println(tempDegreesFahrenheit = (9/5) * degreesCelsius + 32.0);
+
+    }
+}
+
