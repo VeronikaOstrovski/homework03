fa3fc7c8c88383c9fe76542844fdfc465a7e0583
veronikaostrovski <veronika.turkey@gmail.com>
2023-05-27 01:25:22 +0300

    made changes at the formula

--- src/Temperature.java
+++ src/Temperature.java
@@ -4,10 +4,9 @@ public class Temperature {
     public static void main(String[] args) {
 
         double degreesCelsius = 27.0;
-        double degreesFahrenheit = 90.0;
-        double tempDegreesFahrenheit = (9/5) * degreesCelsius + 32.0;
+        double degreesFahrenheit = (9.0/5.0) * degreesCelsius + 32.0;
 
-        System.out.println(tempDegreesFahrenheit = (9/5) * degreesCelsius + 32.0);
+        System.out.println(degreesFahrenheit);
 
     }
 }
