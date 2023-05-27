d010b944762c9c7cda4a6fe5b86593ea42e0d0ef
veronikaostrovski <veronika.turkey@gmail.com>
2023-05-27 23:37:42 +0300

    task 7 done

---
+++ src/Symbols.java
@@ -0,0 +1,11 @@
+public class Symbols {
+
+    public static void main(String[] args) {
+        char a= 'b';
+        char b= 101;
+
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+}
